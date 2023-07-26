5c62eeb15e65dd0b33f0b3e50233e9c01b8a5987
Craig Putzstuck <putzstuck@gmail.com>
2023-07-26 15:46:38 -0400

    corrected styles error in playlist component

--- src/components/Playlists/index.jsx
+++ src/components/Playlists/index.jsx
@@ -7,16 +7,16 @@ import Button from "../Button"
 import content from "./content"
 
 
-const MixingService = () => {
+const PlaylistComponent = () => {
   return (
-    <AboutStyles>
       <Container maxWidth="xl">
         <motion.div
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           transition={{ duration: 2.5 }}
         >
-          <Typography variant="h4" align="center" style={{ color: 'var(--contrastLight3)' }}>Ready to take your music to new heights? Abstraktius Artimus offers professional music mixing and mastering services that elevate your raw recordings into polished, captivating tracks.
+          <Typography variant="h4" align="center" style={{ color: 'var(--contrastLight3)' }}>
+            
           </Typography>
         </motion.div>
         <hr />
@@ -49,8 +49,7 @@ const MixingService = () => {
           arrow={true}
         />
       </Container>  
-    </AboutStyles>
   )
 }
 
-export default MixingService
+export default PlaylistComponent
