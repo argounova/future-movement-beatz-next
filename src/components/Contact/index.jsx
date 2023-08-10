04582f673fac53d829852943a18860db1410b0b2
Craig Putzstuck <putzstuck@gmail.com>
2023-08-10 13:01:39 -0400

    contact component

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,95 +1,208 @@
-import React, { useState, useRef } from 'react'
+import { useState } from 'react'
+import { motion } from 'framer-motion'
 import { ContactStyles } from "./style"
+import {
+  Button,
+  Box,
+  Checkbox,
+  FormGroup,
+  FormControlLabel,
+  TextField,
+  Typography
+} from '@mui/material'
 import emailjs from '@emailjs/browser'
 
 
 const Contact = () => {
-  const [formName, setFormName] = useState('');
-  const [formEmail, setFormEmail] = useState('');
-  const [formMessage, setFormMessage] = useState('');
-  // const [errorMessage, setErrorMessage] = useState('');
-  const form = useRef();
+  const [userName, setUserName] = useState('')
+  const [userEmail, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+  const [playlistSub, setPlaylistSub] = useState(false)
+  const [musicProd, setMusicProd] = useState(false)
+  const [mixMast, setMixMast] = useState(false)
+  const [coverArt, setCoverArt] = useState(false)
+  const [selections, setSelections] = useState([])
+  const [showContactForm, setShowContactForm] = useState(true)
+  const [showLoading, setShowLoading] = useState(false)
+  const [showSuccess, setShowSuccess] = useState(false)
+  const [showFail, setShowFail] = useState(false)
+  const contactReasons = []
+  const templateParams = {
+    user_name: userName,
+    user_email: userEmail,
+    message: message,
+    contact_reasons: contactReasons
+  }
 
-  const handleFormChange = (e) => {
-    const { target } = e;
-    const formType = target.name;
-    const formValue = target.value;
-
-    if (formType === 'from_name') {
-      setFormName(formValue);
-    } else if (formType === 'reply_to') {
-      setFormEmail(formValue);
-    } else {
-      setFormMessage(formValue);
+  const handleSendMessage = () => {
+    setShowContactForm(false)
+    setShowLoading(true)
+    if (playlistSub) {
+      setSelections([
+        selections.push(
+          { interest: ' Playlist Submissions' }
+        )
+      ])
+    }  
+    if (musicProd) {
+      setSelections([
+        selections.push(
+          { interest: ' Music Production' }
+        )
+      ])
+    }  
+    if (mixMast) {
+      setSelections([
+        selections.push(
+          { interest: ' Mixing & Mastering' }
+        )
+      ])
+    }  
+    if (coverArt) {
+      setSelections([
+        selections.push(
+          { interest: ' Custom Cover Art' }
+        )
+      ])
     }
-  };
+    selections.map((selection) => (
+      contactReasons.push(selection.interest)
+    ))
+    emailjs.send('service_kb5ueuo', 'fmb_contact', templateParams, '-rwhcmI-NaGlHpG5q')
+    .then(function(response) {
+        console.log(response)
+        setShowLoading(false)
+        setShowSuccess(true)
+      }, function(error) {
+        console.log(error)
+        setShowFail(true)
+      })
+  }
 
-  const handleFormSubmit = (e) => {
-    e.preventDefault();
-    // if (!validateEmail(formEmail) || !formName) {
-    //   setErrorMessage('Valid email and/or name required!');
-    //   return;
-    // }
-    console.log('testing this function');
-    console.log(form);
-    console.log(form.current);
-    emailjs.sendForm('service_kb5ueuo', 'template_rtzya6p', form.current, '-rwhcmI-NaGlHpG5q')
-      .then((response) => {
-        console.log('SUCCESS!', response.status, response.text);
-      }, (err) => {
-        console.log('FAILED...', err);
-      });
-
-    handleShow();
-    setFormName('');
-    setFormEmail('');
-    setFormMessage('');
-  };
-
-  const [show, setShow] = useState(false);
-  const handleShow = () => setShow(true);
   
   return (
-    <ContactStyles className="section">
+    <ContactStyles>
       <div className='intro__area'>
         <h2 id='contact'>Get in Touch</h2>
       </div>
-
-      <div style={{ display: show ? "block" : "none" }}>
-        <h1>Message Sent!</h1>
-        <p>Thank you for contacting me.  Please allow 2-3 business days for a response.</p>
-      </div>
-
-      <form 
-        name="contact"
-        netlify
-        ref={form}
-        className='form'  
-      >
-        <input
-          placeholder="Your name..."
-          type="text"
-          name="from_name"
-          value={formName}
-          onChange={handleFormChange}  
-        />
-        <input
-          placeholder="Your email..."
-          type="email"
-          name="reply_to"
-          value={formEmail}
-          onChange={handleFormChange}  
-        />
-        <textarea
-          placeholder="Your message..."
-          type='text'
-          name="message"
-          rows="5"
-          value={formMessage}
-          onChange={handleFormChange}
-        />
-        <button type='button' onClick={handleFormSubmit}>Send Message</button>
-      </form>
+      { showContactForm && (
+          <div className='contact-form'>
+              <TextField
+                  fullWidth
+                  required
+                  id="name"
+                  label="Name"
+                  variant="filled"
+                  color="secondary"
+                  onChange={(e) => setUserName(e.target.value)}
+                  sx={{ m: 1 }}
+              />
+              <TextField
+                  fullWidth
+                  required
+                  id="email"
+                  label="Email"
+                  type="email"
+                  variant="filled"
+                  color="secondary"
+                  onChange={(e) => setEmail(e.target.value)}
+                  sx={{ m: 1 }}
+              />
+              <TextField
+                  fullWidth
+                  required
+                  id="message"
+                  label="Message"
+                  variant="filled"
+                  color="secondary"
+                  onChange={(e) => setMessage(e.target.value)}
+                  multiline
+                  minRows={5}
+                  sx={{ m: 1 }}
+              />
+              <Box className='selections'>
+                <Typography variant='h6' align='left' sx={{ color: 'var(--contrastDark1)' }}>
+                  Please select your reason&#40;s&#41; for contacting us today:
+                </Typography>
+                <FormGroup sx={{ color: 'var(--contrastDark1)' }}>
+                  <FormControlLabel 
+                    control={<Checkbox
+                      checked={playlistSub}
+                      onChange={(e) => setPlaylistSub(e.target.checked)}
+                    />}
+                    label="Playlist Submission"
+                  />
+                  <FormControlLabel 
+                    control={<Checkbox 
+                      checked={musicProd}
+                      onChange={(e) => setMusicProd(e.target.checked)}
+                    />} 
+                    label="Music Production" 
+                  />
+                  <FormControlLabel
+                    control={<Checkbox
+                      checked={mixMast}
+                      onChange={(e) => setMixMast(e.target.checked)}
+                    />} 
+                    label="Mixing & Mastering" 
+                  />
+                  <FormControlLabel 
+                    control={<Checkbox 
+                      checked={coverArt}
+                      onChange={(e) => setCoverArt(e.target.checked)}
+                    />} 
+                    label="Custom Cover Art" 
+                  />
+                </FormGroup>
+              </Box>
+              
+              <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ m: 1 }}
+                  style={{ backgroundColor: 'var(--contrastDark3)' }}
+                  onClick={handleSendMessage}
+              >Send</Button>   
+          </div>
+      )}
+      { showLoading && (
+        <div className='contact-form' style={{ justifyContent: 'center', stroke: 'var(--fmbPurple3)' }}>
+          <h2>SENDING...</h2>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 200">
+            <motion.g>
+              <motion.circle
+                initial={{ pathLength: 0 }}
+                animate={{ pathLength: 1 }}
+                transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+                }}
+                cx="150"
+                cy="100"
+                r="75"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+              </motion.circle>
+            </motion.g>
+          </svg>
+        </div>
+      )}
+      { showSuccess && (
+          <div className='contact-form' style={{ justifyContent: 'center' }}>
+              <h2>Thank you for contacting us!</h2>
+              <h4>We will get back to you within 3 business days!</h4>
+          </div>
+      )}
+      { showFail && (
+          <div className='contact-form' style={{ justifyContent: 'center' }}>
+              <h2>Something went wrong...</h2>
+              <h4>Please email us at futuremovementbeatz@gmail.com</h4>
+          </div>
+      )} 
     </ContactStyles>
   )
 }
--- src/components/Contact/style.js
+++ src/components/Contact/style.js
@@ -1,95 +1,28 @@
 import styled from "@emotion/styled"
 
 export const ContactStyles = styled.section`
-  max-width: 750px;
-  box-sizing: content-box;
-  margin-left: auto;
-  margin-right: auto;
+max-width: 750px;
+box-sizing: content-box;
+margin-left: auto;
+margin-right: auto;
 
-  input {
-    height: calc(var(--gap) * 2);
-    margin-bottom: var(--gap);
-  }
-
-  input,
-  textarea {
-    background-color: #000;
-    color: #fff;
-    border: none;
-    outline: 2px solid rgba(255, 255, 255, 0.15);
-    border-radius: 6px;
-    width: 100%;
-    font-size: var(--p);
-    font-weight: 700;
-    font-family: "Heebo", sans-serif;
-    padding: 15px;
-    transition: outline 0.3s ease;
-
-    &:focus {
-      outline: 2px solid var(--primary);
-    }
-
-    &::-webkit-input-placeholder {
-      color: var(--inActive);
-    }
-
-    &::-moz-placeholder {
-      color: var(--inActive);
-    }
-
-    &:-ms-input-placeholder {
-      color: var(--inActive);
-    }
-
-    &:-moz-placeholder {
-      color: var(--inActive);
-    }
-  }
-  textarea {
-    margin-bottom: var(--gap);
-  }
-
-  button {
-    color: #fff;
-    background-color: transparent;
-    font-family: "Heebo", sans-serif;
-    border: 0;
-    text-decoration: none;
-    padding: 0;
-    transition: color 0.3s ease;
-    text-transform: capitalize;
-    font-size: var(--p);
-    font-weight: 700;
-    position: relative;
-    padding-bottom: 15px;
-    display: inline-flex;
+  .contact-form {
+    display: flex; 
+    flex-direction: column; 
     align-items: center;
+    background-color: var(--contrastLight3);
+    width: 100%;
+    height: fit-content;
+    border-radius: 5px;
+    stroke: var(--mb1-1);
+    padding: 20px;
+    color: var(--contrastDark1);
+  }
 
-    &::after {
-      content: "";
-      display: block;
-      position: absolute;
-      height: 3px;
-      left: 0;
-      right: 0;
-      bottom: 8px;
-      background-color: var(--primary);
-      transition: left 0.3s ease;
-    }
-
-    &:focus {
-      color: var(--primary);
-    }
-
-    @media (hover: hover) {
-      &:hover {
-        cursor: pointer;
-        color: var(--primary);
-  
-        &::after {
-          left: 100%;
-        }
-      }
-    }
+  .selections {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: 100%;
   }
 `
